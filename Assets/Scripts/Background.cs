5ed12791ba76f635117be13b2c16efc114457803
Ethan Ruoff <ethanruoff@gmail.com>
2020-02-03 13:45:02 +0100

    Background Finished!

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float timeLeft;
+    Color targetColor;
+    SpriteRenderer rend;
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeLeft <= Time.deltaTime)
+        {
+            // transition complete
+            // assign the target color
+            rend.material.color = targetColor;
+
+            // start a new transition
+            targetColor = new Color(Random.value, Random.value, Random.value);
+            timeLeft = 4.0f;
+        }
+        else
+        {
+            // transition in progress
+            // calculate interpolated color
+            rend.material.color = Color.Lerp(rend.material.color, targetColor, Time.deltaTime / timeLeft);
+
+            // update the timer
+            timeLeft -= Time.deltaTime;
+        }
+    }
+}
