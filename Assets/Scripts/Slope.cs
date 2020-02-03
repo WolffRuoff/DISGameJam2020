953868f672661ceb972fcadf6809d8273443df05
Matt Gropper <mgropps17@gmail.com>
2020-02-03 15:01:15 +0100

    infinite generation

---
+++ Assets/Scripts/Slope.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slope : MonoBehaviour
+{
+
+    public GameObject button;
+    public GameObject denim;
+
+    private GameObject lastObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastObj = denim;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((int) button.transform.position.x % 10 == 0)
+        {
+            // get current button position and keep rotation constant
+            Vector3 pos = lastObj.transform.position;
+            pos.x += 10;
+            pos.y -= 6.3f;
+            Quaternion rot = denim.transform.rotation;
+
+            lastObj = Instantiate(denim, pos, rot);
+        }
+    }
+}
