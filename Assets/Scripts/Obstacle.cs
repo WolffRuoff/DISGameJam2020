f9445c21016bd6a59caa9a54c408d4eda8f9ba4f
Matt Gropper <mgropps17@gmail.com>
2020-02-04 13:29:38 +0100

    final

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    public GameObject needleObstacle;
-
     public float YMin;
     public float YMax;
     public float XMin;
@@ -14,37 +12,26 @@ public class Obstacle : MonoBehaviour
 
     void Start()
     {
-        spawnObj(true);
-        spawnObj(false);
+        Transform needle1 = gameObject.transform.GetChild(2);
+        Transform needle2 = gameObject.transform.GetChild(3);
+        Transform needle3 = gameObject.transform.GetChild(4);
+
+        float x1 = needle1.position.x + Random.Range(XMin, XMax);
+        float y1 = needle1.position.y + Random.Range(YMin, YMax);
+        float x2 = needle2.position.x + Random.Range(XMin, XMax);
+        float y2 = needle2.position.y + Random.Range(YMin, YMax);
+        float x3 = needle3.position.x + Random.Range(XMin, XMax);
+        float y3 = needle3.position.y + Random.Range(YMin, YMax);
+
+        needle1.position = new Vector3(x1, y1, 1);
+        needle2.position = new Vector3(x2, y2, 1);
+        needle3.position = new Vector3(x3, y3, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
 
-    }
-
-    void spawnObj (bool first)
-    {
-        float x;
-        float y = Random.Range(YMin, YMax);
-        float mdpt = (XMin + XMax) / 2;
-
-        if (first)
-        { 
-            x = Random.Range(XMin, mdpt);
-            y = 3;
-        } else
-        {
-            x = Random.Range(mdpt, XMax);
-            y = 1;
-        }
-
-        Vector3 spawnPos = transform.position;
-        spawnPos.x += x;
-        spawnPos.y += y;
-
-        Instantiate(needleObstacle, spawnPos, Quaternion.identity);
     }
     
 }
