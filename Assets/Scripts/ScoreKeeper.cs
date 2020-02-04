c7141b399870ff501d63e3968313827c7003d03e
Josh Londin <jgl92@cornell.edu>
2020-02-04 10:56:51 +0100

    implemented Score

---
+++ Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    public Transform player;
+    public float offsetX;
+    public float offsetY;
+
+    TextMesh text;
+    private float startTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = gameObject.GetComponent<TextMesh>();
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x = player.position.x + offsetX;
+        pos.y = player.position.y + offsetY;
+        transform.position = pos;
+
+        text.text = ""+((Time.time - startTime)*12);
+    }
+}
