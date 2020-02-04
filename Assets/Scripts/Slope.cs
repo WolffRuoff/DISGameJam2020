4946ae7120ed0b04f06515fe99af93197f4a8d7b
Matt Gropper <mgropps17@gmail.com>
2020-02-04 11:31:57 +0100

    fixed slope

--- Assets/Scripts/Slope.cs
+++ Assets/Scripts/Slope.cs
@@ -21,14 +21,14 @@ public class Slope : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if ((int) button.transform.position.x % 15 == 0 && (int)button.transform.position.x > prevX)
+        if ((int) button.transform.position.x % 25 == 0 && (int)button.transform.position.x > prevX)
         {
             prevX = (int)button.transform.position.x;
 
             // get current button position and keep rotation constant
             Vector3 pos = lastObj.transform.position;
-            pos.x += 15;
-            pos.y -= 8f;
+            pos.x += 37.7f;
+            pos.y -= 23.72f;
             Quaternion rot = denim.transform.rotation;
 
             lastObj = Instantiate(denim, pos, rot);
